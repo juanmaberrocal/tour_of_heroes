895cdb025e8fbe8d596a13bf294de6ec65612323
Juan Berrocal <juanmaberrocal37@gmail.com>
2019-03-04 07:32:26 -0600

    init commit

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,9 +1,21 @@
 import 'package:angular/angular.dart';
+import 'package:angular_forms/angular_forms.dart';
+
+import 'src/hero.dart';
+import 'src/mock_heroes.dart';
+import 'src/hero_component.dart';
 
 @Component(
   selector: 'my-app',
-  template: '<h1>Hello {{name}}</h1>',
+  templateUrl: 'app_component.html',
+  styleUrls: ['app_component.css'],
+  directives: [coreDirectives, HeroComponent],
 )
 class AppComponent {
-  var name = 'Angular';
+  final title = 'Tour of Heroes';
+  List<Hero> heroes = mockHeroes;
+
+  Hero selected;
+
+  void onSelect(Hero hero) => selected = hero;
 }
